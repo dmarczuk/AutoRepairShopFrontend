2b271c990de35a057b75b3a6f222109043057207
Dominik <marczukd@gmail.com>
2025-01-13 14:27:50 +0100

    add style for mobile devices

--- src/models/models.ts
+++ src/models/models.ts
@@ -7,14 +7,14 @@ export interface ThemeType {
 }
 
 export interface Client {
-    id: number;
+    clientId: number;
     firstName: string;
     secondName: string;
     phoneNumber: string;
     email: string;
 }
 
-export interface Vehicle {
+export interface Car {
     vehicleRegistration: string;
     mark: string;
     model: string;
@@ -22,10 +22,15 @@ export interface Vehicle {
     vin: string
 }
 
+export interface CarClientDto {
+    car: Car;
+    phoneNumber: string
+}
+
 export interface Mechanic {
-    id: string;
+    mechanicId: number;
     firstName: string;
-    secondName: boolean;
+    secondName: string;
     ifEmployed: string;
     username: string;
     password: string
@@ -34,6 +39,7 @@ export interface Mechanic {
 export interface Repair {
     repairId: string
     mechanic: Mechanic;
+    car: Car;
     startDate: string;
     endDate: string;
     state: string;
@@ -43,9 +49,31 @@ export interface Repair {
 
 }
 
+export interface TicketDto {
+    client: Client;
+    car: Car;
+}
+
+export interface RepairMechanicDto {
+    repairId: number;
+    mechanicUsername: string;
+}
+
+export interface LoginRequest {
+    username: string;
+    password: string
+}
+
 export const formatDate = (dateString: string | null | undefined): string => {
     if (!dateString) {
         return ""; // Return an empty string if the input is null or undefined
     }
     return dateString.slice(0, 10); // Extract YYYY-MM-DD
+};
+
+export const formatUsername = (mechanic: Mechanic | null | undefined): string => {
+    if (!mechanic) {
+        return ""; // Return an empty string if the input is null or undefined
+    }
+    return mechanic.username; // Extract YYYY-MM-DD
 };
